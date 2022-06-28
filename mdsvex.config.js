ae3d593b067ed98d130af45b0c95ad6379b10e77
JakeBruner <74114605+JakeBruner@users.noreply.github.com>
2022-06-28 01:06:05 -0400

    proof of concept of a markdown blog with mdsvex :)

---
+++ mdsvex.config.js
@@ -0,0 +1,25 @@
+import {
+    defineMDSveXConfig as defineConfig
+} from 'mdsvex';
+
+import {
+    fileURLToPath
+} from 'url';
+
+import * as path from 'path';
+
+const dirname = path.resolve(fileURLToPath(
+    import.meta.url), '../');
+
+// https://mdsvex.pngwn.io/docs#smartypants=
+const config = defineConfig({
+    extensions: ['.md', '.svx'],
+    smartypants: {
+        dashes: 'oldschool',
+    },
+    layout: {
+        blog: path.join(dirname, './src/routes/blog/_layout.svelte')
+    }
+});
+
+export default config;
---
+++ src/routes/blog/index.js
@@ -0,0 +1,34 @@
+import {
+    convertDate
+} from '$lib/utils';
+
+export async function get() {
+    const allPostFiles =
+        import.meta.globEager('./*/*.{svx,md}'); // this should be made page-speceifc
+    // "exposes context-specific metadata to a JavaScript module"
+    // in this case its the metadata within the --- header
+    const allPosts = Object.entries(allPostFiles).map(([path, post]) => {
+        // maps each post to this mega object
+        const postPath = path.slice(2, -3);
+        // slice takes off the ./ and .md
+        return {
+            ...post.metadata,
+            path: postPath,
+            published: convertDate(post.metadata.date)
+        };
+    });
+
+    // TODO add other sorting methods/options
+    const posts = allPosts.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+    if (!posts.length) { // if no posts
+        return {
+            status: 404
+        };
+    }
+    return {
+        body: {
+            posts
+        }
+    };
+}
