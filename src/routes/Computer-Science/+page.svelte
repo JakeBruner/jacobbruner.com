75c8ac06060ce85353482b892606c937f8101467
JakeBruner <74114605+JakeBruner@users.noreply.github.com>
2022-09-08 00:20:53 -0400

    slightly nicer styles for cs page

--- src/routes/Computer-Science/+page.svelte
+++ src/routes/Computer-Science/+page.svelte
@@ -23,16 +23,20 @@
     "
       >
         <details
-          class="open:bg-white dark:open:bg-zinc-900 open:ring-1 open:ring-black/5 open:py-3 open:pl-1.5 open:pr-3 dark:open:ring-zinc-300/10 dark:open:shadow-white/5 open:shadow-2xl open:rounded-xl transition-all ease-in"
+          class="open:bg-white dark:open:bg-zinc-900 open:ring-1 open:ring-black/5 open:py-3 open:pl-1.5 open:pr-3 dark:open:ring-zinc-300/10 dark:open:shadow-white/5 open:shadow-lg open:rounded-md open:lg:rounded-xl transition-all ease-in"
         >
           <!-- title="there also exists a coffee functor... Coffee :: (Monad m) => x -> m x, from which this website is a corolarry of a proof of it's existence" -->
-          <summary class="select-none"> CS :: Jacob → [String] </summary>
-          <p class="p-5 text-xl dark:text-zinc-300 text-zinc-500 font-medium leading-relaxed">
+          <summary class="select-none hover:translate-x-0.5 transition ease-in-out">
+            CS :: Jacob → [String]
+          </summary>
+          <p
+            class="p-5 text-lg lg:text-xl tracking-wide leading-tight dark:text-zinc-300 font-light text-zinc-500"
+          >
             There also exists a coffee endofunctor, Coffee :: (Monad m) => x -> m x, from which this
-            website is a corollary of it's existence. Additionally there is a natural transformation
-            η :: a . Coffee a -> Alcohol a, where Alcohol is a contravarient functor to the Lazy
-            category. This forms a 'dinatural' Transformation ☺. Unfortunately the Alcohol functor
-            has side-effects (namely liver damage), so the diagram techincally wouldn't commute.
+            website is a corollary of its existence. Additionally there is a natural transformation
+            η :: a . Coffee a -> Alcohol a, where Alcohol is a contravariant functor to the Lazy
+            category. This forms a 'dinatural' Transformation :) Unfortunately the Alcohol functor
+            has side-effects (namely liver damage), so the diagram technically wouldn't commute...
           </p>
         </details>
       </h2>
