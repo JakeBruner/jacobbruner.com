afd88d8e248903d3790a29a00ea45f1d7ab82767
JakeBruner <74114605+JakeBruner@users.noreply.github.com>
2022-10-28 14:53:05 -0400

    new image component for lazy loading

---
+++ src/components/LazyImage.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  export let src: string;
+  export let alt: string;
+  import { onMount } from "svelte";
+  import { fade } from "svelte/transition";
+
+  let image: HTMLImageElement;
+  let loaded = false;
+  let error = false;
+  onMount(() => {
+    image.onload = () => {
+      loaded = true;
+    };
+    image.onerror = () => {
+      error = true;
+    };
+  });
+</script>
+
+{#if !loaded && !error}
+  <div class="w-full h-full bg-zinc-100 rounded-lg shadow-sm animate-pulse" />
+{/if}
+
+{#if loaded}
+  <img
+    class="block object-cover object-center w-full h-full rounded-lg shadow-sm"
+    {src}
+    {alt}
+    usemap="#image-map"
+    transition:fade
+    class:loaded
+    bind:this={image}
+  />
+{/if}
+
+{#if error}
+  <div class="w-full h-full rounded-lg shadow-sm bg-zinc-100" />
+{/if}
--- src/routes/Photography/Kilimanjaro/+page.svelte
+++ src/routes/Photography/Kilimanjaro/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import { fade } from "svelte/transition";
-  // import { onMount } from "svelte";
+  import LazyImage from "$components/LazyImage.svelte";
+
+  // intersection observer
+  let observer: IntersectionObserver;
 
   let fullscreen: boolean = false;
 
@@ -57,11 +60,86 @@
         });
       }
     };
+    // log src and alt of all images
+    let test = [];
+    images.forEach((image) => {
+      // get rid of https://127.0.0.1:4000/
+
+      const src = image.src.replace("http://127.0.0.1:4000", "");
+      console.log(`src: "${src}", alt: "${image.alt}"`);
+    });
   });
+
+  const photoList: Photo[] = [
+    {
+      src: "/images/kili/kilimanjaro-2465.webp",
+      alt: "looking towards the receding 'Rebman Glacier' from the summit"
+    },
+    { src: "/images/kili/kilimanjaro-1747.webp", alt: "group pic at one of our first camps" },
+    {
+      src: "/images/kili/kilimanjaro-2058.webp",
+      alt: "our family friends from jackson hole looking cheerful"
+    },
+    {
+      src: "/images/kili/kilimanjaro-2433.webp",
+      alt: "looking up towards the long summit hike from School Hut camp"
+    },
+    { src: "/images/kili/kilimanjaro-2258.webp", alt: "some kind of cool bird" },
+    {
+      src: "/images/kili/kilimanjaro-2447.webp",
+      alt: "the start of our summit trek at ~12 midnight"
+    },
+    {
+      src: "/images/kili/kilimanjaro-1983.webp",
+      alt: "early morning sunrise from the first campsite on the mountain's plateau"
+    },
+    { src: "/images/kili/kilimanjaro-jake.webp", alt: "me at the summit" },
+    {
+      src: "/images/kili/kilimanjaro-2461.webp",
+      alt: "sunrise coming up as we make our final steps to the summit sign"
+    },
+    { src: "/images/kili/kilimanjaro-2080.webp", alt: "one of our mountain guides ontop a rock" },
+    {
+      src: "/images/kili/kilimanjaro-2190.webp",
+      alt: "me and our sick guide Dominique on a side-hike"
+    },
+    {
+      src: "/images/kili/kilimanjaro-2248.webp",
+      alt: "our group getting ready to hike in the morning from the breakfast tent"
+    },
+    {
+      src: "/images/kili/kilimanjaro-2344.webp",
+      alt: "sunrise over kilimanjaro on one of our far-out Northern-Circuit route camps"
+    },
+    {
+      src: "/images/kili/kilimanjaro-1942.webp",
+      alt: "milky way overtop one of our plateau campsites early in the morning"
+    },
+    {
+      src: "/images/kili/kilimanjaro-1827.webp",
+      alt: "a typical trail on kilimanjaro from the rainforest to the plateau zone"
+    },
+    {
+      src: "/images/kili/kilimanjaro-2464.webp",
+      alt: "the view of the melting snowpack about 300 yards from the summit sign"
+    },
+    {
+      src: "/images/kili/kilimanjaro-2282.webp",
+      alt: "the final steps to our 3rd to summit campsite"
+    },
+    {
+      src: "/images/kili/kilimanjaro-1753.webp",
+      alt: "late night gathering for dinner within the food tent"
+    },
+    {
+      src: "/images/kili/kilimanjaro-.webp",
+      alt: "first group to reach the top at 6:30 am. I'm on the bottom left!"
+    }
+  ];
 </script>
 
-<svelte:body class:overflow-y={fullscreen ? "hidden" : "auto"} />
-<svelte:window on:keydown={() => deselectMe()} />
+<!-- <svelte:body class:overflow-y={fullscreen ? "hidden" : "auto"} /> -->
+<svelte:window on:keydown={deselectMe} />
 
 <div class="py-6 text-center">
   <h1 class="py-1">Kilimanjaro</h1>
@@ -88,11 +166,7 @@
     <div class="flex flex-wrap -m-1.5 md:-m-3">
       <!-- gallery container -->
       <div class="w-full photoframe">
-        <img
-          alt="looking towards the receding 'Rebman Glacier' from the summit"
-          class="img"
-          src="/images/kili/kilimanjaro-2465.webp"
-        />
+        <LazyImage src={photoList[0].src} alt={photoList[0].alt} />
       </div>
 
       <div class="flex flex-wrap md:w-1/2 w-full">
