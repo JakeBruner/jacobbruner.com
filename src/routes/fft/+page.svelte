b2454433bc0e7b407fd32135b7c9b9a3ab106d58
JakeBruner <74114605+JakeBruner@users.noreply.github.com>
2022-12-15 18:30:54 -0500

    thing :?

--- src/routes/fft/+page.svelte
+++ src/routes/fft/+page.svelte
@@ -1,48 +1,40 @@
 <script lang="ts">
   // audio-fft.ts
-  import { onMount } from 'svelte';
-  import { error } from '@sveltejs/kit'
-  import AudioFFT from './audio';
+  import { onMount } from "svelte";
+  import { error } from "@sveltejs/kit";
+  import AudioFFT from "./audio";
 
   let analyser: AnalyserNode;
   let audioContext: AudioContext;
-  
 
   const initAudio = async () => {
-    console.log('navigator.mediaDevices', navigator.mediaDevices);
+    console.log("navigator.mediaDevices", navigator.mediaDevices);
     if (!navigator.mediaDevices) {
-      throw error(500, 'MediaDevices not supported');
+      throw error(500, "MediaDevices not supported");
     }
 
-    const stream = await navigator.mediaDevices
-      .getUserMedia({
-        audio: true,
-      })
-      
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true
+    });
+
     // Create an AudioNode from the stream
-    console.log('stream', stream)
+    console.log("stream", stream);
     audioContext = new AudioContext();
     const audioSource = audioContext.createMediaStreamSource(stream);
     analyser = audioContext.createAnalyser();
     audioSource.connect(analyser);
 
-
-
     if (!analyser) {
-      throw error(500, 'Analyser not supported');
+      throw error(500, "Analyser not supported");
     }
     const audioFFT = new AudioFFT(c, analyser);
-
-   
   };
 
   onMount(() => {
     console.log(navigator.mediaDevices?.enumerateDevices());
-
   });
 
-   let c: HTMLCanvasElement;
-
+  let c: HTMLCanvasElement;
 </script>
 
 <svelte:head>
@@ -51,8 +43,12 @@
 
 <h1>Real-time Audio FFT</h1>
 <div class="container flex">
-  
-  <canvas bind:this={c} class="mx-auto  dark:stroke-slate-100 dark:fill-zinc-300" width="1024px" height="512px"></canvas>
+  <canvas
+    bind:this={c}
+    class="mx-auto stroke-black  dark:stroke-slate-100 dark:fill-zinc-300 bg-white"
+    width="1024px"
+    height="512px"
+  />
 </div>
 
 <button on:click={initAudio}>Start</button>
