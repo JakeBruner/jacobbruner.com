09c6af21999049a400459b482ed3adbe88ce37d4
JakeBruner <74114605+JakeBruner@users.noreply.github.com>
2023-01-19 19:26:49 -0500

    style ToneUI component

--- src/components/tonegen/ToneUI.svelte
+++ src/components/tonegen/ToneUI.svelte
@@ -60,9 +60,6 @@
           ? 0
           : -180
         : Math.atan2(diffY, diffX) * (180 / Math.PI);
-    // console.log(diffY, diffY === 0);
-
-    // console.log(angle, angle <= 0);
 
     if ((angle >= -180 && angle <= 0) || Math.trunc(angle) === 180) {
       tone.panNode.pan.value = (angle * (10 / 9) + 100) / 100;
@@ -85,15 +82,21 @@
 
   let storedGain: number | null = null;
 
-  let popupActive = false;
-  $: console.log(popupActive);
+  let popupActive = true;
+  // $: console.log(popupActive);
 
-  const togglePopup = () => {
+  const togglePopup = (e: MouseEvent | KeyboardEvent) => {
+    e.stopPropagation();
     popupActive = !popupActive;
+
     if (popupActive) {
-      window.addEventListener("click", () => {
-        popupActive = false;
-      });
+      window.addEventListener(
+        "click",
+        () => {
+          popupActive = false;
+        },
+        { once: true }
+      );
     } else {
       window.removeEventListener("click", () => {
         popupActive = false;
@@ -121,13 +124,13 @@
   // let pendingFrequency: string;
 
   // $: pendingFrequency = tone.oscNode.frequency.value.toString();
-  console.log("test C4", getNoteFrequency("A4"));
+  // console.log("test C4", getNoteFrequency("A4"));
 </script>
 
 {#if tone}
   <div class="shadow-md">
     <div
-      class="w-full bg-gradient-to-tr from-zinc-700 via-zinc-700 to-zinc-600 shadow-inner z-0 rounded-2xl flex flex-row-reverse justify-between align-middle "
+      class="w-full bg-gradient-to-tr from-zinc-200 via-zinc-100 to-zinc-300  dark:from-zinc-700 dark:via-zinc-700 dark:to-zinc-600 shadow-inner z-0 rounded-2xl flex flex-row-reverse justify-between align-middle "
     >
       <div
         class={c(
@@ -137,37 +140,47 @@
         on:click|self={togglePopup}
         on:keydown|self={(e) => {
           if (e.key === "Enter") {
-            togglePopup();
+            togglePopup(e);
           }
         }}
       >
         <Plus
           variation="solid"
-          on:click={() => {
-            // console.log("clicked plus");
-            popupActive = !popupActive;
+          on:click={(e) => {
+            togglePopup(e);
           }}
-          class={c("ml-0.5 my-auto h-6 w-6", popupActive ? "text-zinc-400" : "text-zinc-300")}
+          class={c(
+            "ml-0.5 my-auto h-6 w-6",
+            popupActive ? "text-zinc-800 dark:text-zinc-400" : "dark:text-zinc-300 text-zinc-600"
+          )}
         />
         <div class="overflow-hidden ">
           {#if popupActive}
             <!-- svelte-ignore a11y-click-events-have-key-events -->
             <div
-              class="absolute z-50 p-2 w-40 top-1/2 right-0 rounded-md bg-zinc-400/80 backdrop-blur-sm"
+              class="absolute z-50 p-2 w-40 top-1/2 right-0 rounded-md dark:bg-zinc-400/80 bg-zinc-300/50 backdrop-blur-sm"
               transition:fade
               on:click|stopPropagation
             >
               <table class="z-50 w-full table-fixed">
                 <thead>
                   <tr>
-                    <th colspan="3" class="text-zinc-50 text-lg font-semibold text-center"
+                    <th
+                      colspan="3"
+                      class="dark:text-zinc-50 text-zinc-700 text-lg font-semibold text-center"
                       >Add interval</th
                     >
                   </tr>
                   <tr>
-                    <th class="text-zinc-50 text-sm font-medium text-center">Equal</th>
-                    <th class="text-zinc-50 text-sm font-medium text-center">Just</th>
-                    <th class="text-zinc-50 text-sm font-medium text-center">Pyth.</th>
+                    <th class="dark:text-zinc-50 text-zinc-600 text-sm font-medium text-center"
+                      >Equal</th
+                    >
+                    <th class="dark:text-zinc-50 text-zinc-600 text-sm font-medium text-center"
+                      >Just</th
+                    >
+                    <th class="dark:text-zinc-50 text-zinc-600 text-sm font-medium text-center"
+                      >Pyth.</th
+                    >
                   </tr>
                 </thead>
                 <tbody>
@@ -176,40 +189,48 @@
                       <td class="font-medium text-center text-base font-mono">
                         <button
                           class={c(
-                            "w-7 p-px rounded-md bg-zinc-500 hover:bg-zinc-500/75 text-zinc-50 hover:text-zinc-100",
+                            "w-7 p-px rounded-md bg-zinc-400 hover:shadow dark:bg-zinc-500 hover:dark:bg-zinc-500/75 hover:bg-zinc-400/90 dark:text-zinc-50 text-white hover:text-zinc-100",
                             interval.name === "diminished fifth" && "hidden",
                             interval.name === "augmented fourth" && "translate-y-3"
                           )}
                           class:hidden={interval.name === "diminished fifth"}
                           title={"equal tempered " + interval.name}
-                          on:click={() => {
-                            console.log("click");
-                            popupActive = false;
+                          on:click={(e) => {
+                            togglePopup(e);
                             spawnChild(tone, interval, "equal");
-                          }}>{interval.unicode}</button
+                          }}
+                          ><span class="block translate-y-px -translate-x-px"
+                            >{interval.unicode}</span
+                          ></button
                         >
                       </td>
                       <td class="font-medium text-center text-base font-mono">
                         <button
-                          class="w-7 p-px rounded-md bg-zinc-500 hover:bg-zinc-500/75 text-zinc-50 hover:text-zinc-100"
+                          class="w-7 p-px rounded-md bg-zinc-400 hover:shadow dark:bg-zinc-500 hover:dark:bg-zinc-500/75 hover:bg-zinc-400/90 dark:text-zinc-50 text-white hover:text-zinc-100"
                           title={"just tempered " + interval.name}
                           class:hidden={interval.just === interval.equal}
-                          on:click={() => {
-                            popupActive = false;
+                          on:click={(e) => {
+                            togglePopup(e);
                             spawnChild(tone, interval, "just");
-                          }}>{interval.unicode}</button
+                          }}
+                          ><span class="block translate-y-0.5 -translate-x-px"
+                            >{interval.unicode}</span
+                          ></button
                         >
                       </td>
                       <td class="font-medium text-center text-base font-mono">
                         <button
-                          class="w-7 p-px rounded-md bg-zinc-500 hover:bg-zinc-500/75 text-zinc-50 hover:text-zinc-100"
+                          class="w-7 p-px rounded-md bg-zinc-400 hover:shadow dark:bg-zinc-500 hover:dark:bg-zinc-500/75 hover:bg-zinc-400/90 dark:text-zinc-50 text-white hover:text-zinc-100"
                           title={"pythagoran tempered " + interval.name}
                           class:hidden={interval.pyth === interval.equal ||
                             interval.pyth === interval.just}
-                          on:click={() => {
-                            popupActive = false;
+                          on:click={(e) => {
+                            togglePopup(e);
                             spawnChild(tone, interval, "pyth");
-                          }}>{interval.unicode}</button
+                          }}
+                          ><span class="block translate-y-0.5 -translate-x-px"
+                            >{interval.unicode}</span
+                          ></button
                         >
                       </td>
                     </tr>
@@ -222,12 +243,15 @@
       </div>
       <div
         class={c(
-          "rounded-2xl flex flex-row px-6 py-4 items-center align-middle z-0 transition-all duration-150 ease-in-out dark:bg-zinc-750 shadow-inner relative",
-          popupActive ? "w-[94%] -z-10" : "w-[96%] peer-hover:w-[94%]"
+          "rounded-2xl flex flex-row px-6 py-4 items-center align-middle z-0 transition-all duration-150 ease-in-out bg-zinc-300 dark:bg-zinc-750 shadow-inner relative",
+          popupActive ? "w-[94%] -z-10 rounded-r-3xl" : "w-[96%] peer-hover:w-[94%]"
         )}
       >
         <div class="absolute top-0 left-0 z-30 p-2">
-          <XMark class="h-4 w-4 text-zinc-400" on:click={() => removeTone(tone.id)} />
+          <XMark
+            class="h-4 w-4 text-zinc-650 dark:text-zinc-400"
+            on:click={() => removeTone(tone.id)}
+          />
         </div>
         <!-- volume slider -->
 
@@ -235,7 +259,7 @@
           {#if tone.gainNode.gain.value === 0}
             <SpeakerXMark
               variation="solid"
-              class="h-4 w-4 mr-2 text-zinc-300"
+              class="h-4 w-4 mr-2 dark:text-zinc-300 text-zinc-700"
               on:click={() => {
                 tone.gainNode.gain.value = storedGain ?? 0.5;
                 storedGain = null;
@@ -244,7 +268,7 @@
           {:else}
             <SpeakerWave
               variation="solid"
-              class="h-4 w-4 mr-2 text-zinc-300"
+              class="h-4 w-4 mr-2 dark:text-zinc-300 text-zinc-700"
               on:click={() => {
                 storedGain = tone.gainNode.gain.value;
                 tone.gainNode.gain.value = 0;
@@ -254,7 +278,7 @@
         </button>
         <div
           class={c(
-            "flex-grow max-w-3xs border-2 border-zinc-400 rounded-sm h-5 relative z-10",
+            "flex-grow max-w-3xs border-2 border-zinc-700 dark:border-zinc-400 rounded-sm h-5 relative z-10",
             tone.gainNode.gain.value === 0 ? "cursor-not-allowed" : "cursor-pointer"
           )}
           bind:this={volumeParent}
@@ -263,7 +287,7 @@
             style:width={(tone.gainNode.gain.value || storedGain || 0) * 100 + "%"}
             bind:this={volume}
             class={c(
-              "bg-zinc-950 h-[1.04rem] absolute -z-10 left-0",
+              "dark:bg-zinc-950 bg-zinc-400 h-[1.04rem] absolute -z-10 left-0",
               tone.gainNode.gain.value === 0 && "opacity-50 "
             )}
             on:mousedown={() => {
@@ -284,7 +308,7 @@
         <!-- TODO handle disabled state -->
         <input
           type="alphanumeric"
-          class="w-24 rounded-md bg-zinc-700 text-lg text-center touch-none"
+          class="w-24 rounded-md bg-zinc-200 dark:bg-zinc-700 text-lg text-center touch-none dark:text-zinc-300 text-zinc-800"
           value={tone.oscNode.frequency.value}
           on:change={(e) => {
             // try to parse the input as a number
@@ -309,12 +333,12 @@
         <div class="px-2" />
         <!-- double frequency button -->
         <button
-          class="py-1 px-1 mr-1 rounded-md bg-zinc-500 hover:bg-zinc-600 text-sm text-center touch-none text-zinc-300"
+          class="py-1 px-1 mr-1 rounded-md bg-zinc-400 dark:bg-zinc-500 hover:bg-zinc-600 text-sm text-center touch-none text-zinc-50 dark:text-zinc-300"
           title="double frequency"
           on:click={() => (tone.oscNode.frequency.value *= 2)}>2x</button
         >
         <button
-          class="py-1 px-1 rounded-md bg-zinc-500 hover:bg-zinc-600 text-sm text-center touch-none text-zinc-300"
+          class="py-1 px-1 rounded-md bg-zinc-400 dark:bg-zinc-500 hover:bg-zinc-600 text-sm text-center touch-none text-zinc-50 dark:text-zinc-300"
           title="half frequency"
           on:click={() => (tone.oscNode.frequency.value /= 2)}>½x</button
         >
@@ -323,20 +347,22 @@
         <div class="flex flex-col translate-y-1" title="pan stereo channels">
           <div class="relative w-10 h-10 block">
             <div
-              class="rounded-full w-full h-full border-2 border-zinc-300"
+              class="rounded-full w-full h-full border-2 dark:border-zinc-300 border-zinc-500"
               bind:this={dial}
               on:mousedown={handleMouseDown}
             >
               <div
-                class="absolute w-1 h-1/2 bg-zinc-100 origin-bottom bottom-1/2 left-[18.5px] rounded-sm z-10"
+                class="absolute w-1 h-1/2 bg-zinc-600 dark:bg-zinc-100 origin-bottom bottom-1/2 left-[18.5px] rounded-sm z-10"
                 bind:this={indicator}
               />
               <div
-                class="rounded-full absolute top-0.5 left-0.5 w-[90%] h-[90%] border-4 border-dotted border-zinc-400/80 -z-10 rotate-15"
+                class="rounded-full absolute top-0.5 left-0.5 w-[90%] h-[90%] border-4 border-dotted dark:border-zinc-400/80 border-zinc-500 -z-10 rotate-15"
               />
             </div>
           </div>
-          <div class="mx-auto bg-zinc-800 rounded-md px-1 mt-1 text-xs">
+          <div
+            class="mx-auto bg-zinc-200 dark:bg-zinc-800 text-zinc-700 rounded-md px-1 mt-1 text-xs"
+          >
             {Math.trunc(tone.panNode.pan.value * 100)}
           </div>
         </div>
