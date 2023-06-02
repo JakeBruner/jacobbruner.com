fe3096c4b26da6796e83d398370c7325713ccb7b
JakeBruner <74114605+JakeBruner@users.noreply.github.com>
2023-06-02 03:38:05 -0400

    working render of mandelbulb rotating in 3d

--- src/routes/mandelbrot/+page.svelte
+++ src/routes/mandelbrot/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-  import { onMount } from "svelte";
+  import { onMount, onDestroy } from "svelte";
+
+  import fragmentSource from "./raymarch.glsl?raw";
 
   import { GPUComposer, GPULayer, GPUProgram, renderAmplitudeProgram } from "gpu-io";
   //! due to an issue in node_modules, the types entrypoint must be modified from "dist/types/index.d.ts" to "dist/gpu-io.d.ts" to reflect the actual location of the types file
@@ -8,94 +10,64 @@
 
   let canvas: HTMLCanvasElement;
   let composer: GPUComposer;
+  let running: boolean;
 
   onMount(() => {
     composer = new GPUComposer({ canvas, verboseLogging: false });
 
     const raymarchingProgram = new GPUProgram(composer, {
       name: "raymarch",
-      fragmentShader: `
-        in vec2 v_uv;
-        uniform vec3 cameraPosition;
-        uniform float time;
-        const int MAX_ITERATIONS = 256;
-        const float HIT_THRESHOLD = 0.001;
-        const int MAX_STEPS = 256;
-
-        // Function to compute the distance to the fractal surface
-        float mandelbulb(vec3 position) {
-          vec3 z = position;
-          float dr = 1.0;
-          float r = 0.0;
-          for (int i = 0; i < MAX_ITERATIONS; i++) {
-            r = length(z);
-            if (r > 2.0) break;
-            // Compute the new position and derivative
-            float theta = acos(z.z / r);
-            float phi = atan(z.y, z.x);
-            dr = pow(r, 7.0) * 8.0 * dr + 1.0;
-            float zr = pow(r, 8.0);
-            theta = theta * 8.0;
-            phi = phi * 8.0;
-            z = zr * vec3(sin(theta) * cos(phi), sin(phi) * sin(theta), cos(theta)) + position;
-          }
-          return 0.5 * log(r) * r / dr;
-        }
-
-        out vec4 fragColor;
-
-        void main() {
-          vec3 rayOrigin = cameraPosition;
-          rayOrigin.x += cos(time); // Rotate the camera on the x-axis
-          rayOrigin.y += sin(time); // Rotate the camera on the y-axis
-          vec3 rayDirection = normalize(vec3(v_uv * 2.0 - 1.0, 1.0));
-          
-          float t = 0.0;
-          for (int i = 0; i < MAX_STEPS; i++) {
-            vec3 position = rayOrigin + rayDirection * t;
-            float distance = mandelbulb(position);
-            if (distance < HIT_THRESHOLD) {
-              // Compute the color based on the hit
-              fragColor = vec4(vec3(distance), 1.0);
-              // fragColor = vec4(vec3(1.0), 1.0);
-              return;
-            }
-            t += distance;
-          }
-          fragColor = vec4(1.0,1.0,1.0,1.0); // Background color
-        }`,
+      fragmentShader: fragmentSource,
       uniforms: [
+        // {
+        //   name: "time",
+        //   value: 0.0,
+        //   type: "FLOAT"
+        // },
         {
-          name: "time",
-          value: 0.0,
+          name: "cameraPosition",
+          value: [0, 0, -1],
           type: "FLOAT"
         },
         {
-          name: "cameraPosition",
-          value: [0, 0, -5],
+          name: "cameraTarget",
+          value: [0, 0, 0],
+          type: "FLOAT"
+        },
+        {
+          name: "u_resolution",
+          value: [canvas.width, canvas.height],
+          type: "FLOAT"
+        },
+        {
+          name: "u_fov",
+          value: Math.PI / 4, // 45 degrees FOV
           type: "FLOAT"
         }
       ]
     });
 
+    running = true;
     const animate = () => {
-      const time = performance.now() / 10000; // Time in seconds
-      raymarchingProgram.setUniform("time", time, "FLOAT");
-      // raymarchingProgram.uniforms.cameraPosition = [Math.cos(time) * 5, 0, Math.sin(time) * 5 - 5];
-      raymarchingProgram.setUniform(
-        "cameraPosition",
-        [Math.cos(time) * 5, 0, Math.sin(time) * 5 - 5],
-        "FLOAT"
-      );
+      const time = performance.now() / 1000; // Time in seconds
+      // raymarchingProgram.setUniform("time", time, "FLOAT");
+
+      const cameraPosition = [Math.sin(time) * 3, 0, Math.cos(time) * 3];
+      raymarchingProgram.setUniform("cameraPosition", cameraPosition, "FLOAT");
 
       composer.step({
         program: raymarchingProgram
       });
 
-      requestAnimationFrame(animate);
+      running && setTimeout(animate, 10);
+      // requestAnimationFrame(animate);
     };
 
     animate();
+  });
+
+  onDestroy(() => {
+    running = false;
   });
 </script>
 
---
+++ src/routes/mandelbrot/raymarch.glsl
@@ -0,0 +1,67 @@
+in vec2 v_uv;
+uniform vec3 cameraPosition;
+uniform vec3 cameraTarget;
+uniform float time;
+uniform float u_fov;
+uniform vec2 u_resolution;
+const int MAX_ITERATIONS = 128;
+const float HIT_THRESHOLD = 0.001;
+const int MAX_STEPS = 256;
+
+// Function to compute the distance to the fractal surface
+float mandelbulb(vec3 position) {
+  vec3 z = position;
+  float dr = 1.0;
+  float r = 0.0;
+  for (int i = 0; i < MAX_ITERATIONS; i++) {
+    r = length(z);
+    if (r > 2.0) break;
+    // Compute the new position and derivative
+    float theta = acos(z.z / r);
+    float phi = atan(z.y, z.x);
+    dr = pow(r, 7.0) * 8.0 * dr + 1.0;
+    float zr = pow(r, 8.0);
+    theta = theta * 8.0;
+    phi = phi * 8.0;
+    z = zr * vec3(sin(theta) * cos(phi), sin(phi) * sin(theta), cos(theta)) + position;
+  }
+  return 0.5 * log(r) * r / dr;
+}
+
+out vec4 fragColor;
+
+vec4 hsvtorgb(vec3 c) {
+  vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+  vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+  return vec4(c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y), 1.0);
+}
+
+void main() {
+  // Compute the ray direction 
+  vec3 forward = normalize(cameraTarget - cameraPosition);
+  vec3 right = cross(vec3(0.0, 1.0, 0.0), forward); 
+  vec3 up = cross(forward, right); 
+  vec2 uv = v_uv * 2.0 - 1.0;
+  uv.x *= u_resolution.x / u_resolution.y; 
+  vec3 rayDirection = normalize(uv.x * right + uv.y * up + 1.0 / tan(u_fov / 2.0) * forward);
+
+  vec3 rayOrigin = cameraPosition;
+  
+  float t = 0.0;
+  for (int i = 0; i < MAX_STEPS; i++) {
+      vec3 position = rayOrigin + rayDirection * t;
+      float distance = mandelbulb(position);
+      if (distance < HIT_THRESHOLD) {
+        // Compute the color based on the hit
+        float normalizedDistance = distance / HIT_THRESHOLD; // Normalize the distance to range [0, 1]
+        // an interesting, colorful color mapping based on distance
+        fragColor = hsvtorgb(vec3(pow(normalizedDistance,2.0), 0.7, 0.9));
+        
+      
+        return;
+      }
+      t += distance;
+    }
+    fragColor = vec4(0.0, 0.0, 0.0, 1.0); // Background color
+  
+}
