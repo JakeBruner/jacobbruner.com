6a8b0f6b0ca1a0d66d8b5769aa0af2af7c5c2b35
Jake Bruner <jacob.bruner@duke.edu>
2025-10-26 15:28:11 -0400

    change to pnpm

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,24 @@
+/* eslint-env node */
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
-  plugins: ["svelte3", "@typescript-eslint"],
-  ignorePatterns: ["*.cjs"],
-  overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
-  settings: {
-    "svelte3/typescript": () => require("typescript")
-  },
+  parser: 'svelte-eslint-parser',
   parserOptions: {
-    sourceType: "module",
-    ecmaVersion: 2020
+    parser: '@typescript-eslint/parser',
+    extraFileExtensions: ['.svelte'],
+    sourceType: 'module',
+    ecmaVersion: 'latest',
   },
-  env: {
-    browser: true,
-    es2017: true,
-    node: true
-  }
+  plugins: ['svelte', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:svelte/recommended',
+    'prettier'
+  ],
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser'
+    }
+  ]
 };
